1b5611188c7cfdc840330fdab0d8db1070a33b2f
juancgalueweb <juancgalue@icloud.com>
2021-09-27 16:24:40 -0300

    feat: :tada: initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Boxes } from "./components/Boxes";
 
 function App() {
+  const initialProps = {
+    bgColor: "",
+    width: "",
+    height: "",
+  };
+  const [input, setInput] = useState(initialProps);
+  const { bgColor, width, height } = input;
+
+  const [list, setList] = useState([]);
+
+  const handleOnChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setList([...list, input]);
+    setInput(initialProps);
+  };
+  // console.log(input);
+  // console.log("Result", list);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="app" onSubmit={handleSubmit}>
+      <p className="leftAlign">
+        {" "}
+        <b>It is a box generator.</b> Write the name of the color in English. If
+        you don't write anything in the Width and Height field, by default the
+        dimensions of the quadrilateral will be 100px X 100px
+      </p>
+      <label htmlFor="color">Color</label>
+      <input
+        type="text"
+        id="color"
+        name="bgColor"
+        value={bgColor}
+        onChange={handleOnChange}
+      />
+      <label htmlFor="width">Width</label>
+      <input
+        type="text"
+        id="width"
+        name="width"
+        value={width}
+        onChange={handleOnChange}
+      />
+      <label htmlFor="height">Height</label>
+      <input
+        type="text"
+        id="height"
+        name="height"
+        value={height}
+        onChange={handleOnChange}
+      />
+      <button type="submit">Add</button>
+      <Boxes divProps={list} />
+    </form>
   );
 }
 
---
+++ src/components/Boxes.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Boxes = ({ divProps }) => {
+  // console.log(divProps);
+  return (
+    <div className="container">
+      {divProps.length > 0 &&
+        divProps.map((ele, index) => (
+          <div
+            key={index}
+            className="boxes"
+            style={{
+              backgroundColor: ele.bgColor,
+              width: ele.width !== "" ? `${ele.width}px` : `100px`,
+              height: ele.height !== "" ? `${ele.width}px` : `100px`,
+            }}
+          ></div>
+        ))}
+    </div>
+  );
+};
